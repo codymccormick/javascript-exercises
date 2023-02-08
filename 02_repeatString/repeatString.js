c4e17e4c6406ad7c5829fd8b26f88b4382eed51a
Cody McCormick <mr.codymccormick@gmail.com>
2023-02-07 20:19:51 -0600

    Added a function that repeats a string a givin number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    while (num > 0){
+        repeatedString += string;
+        num--;
+    } if (num < 0) {
+        return "ERROR";
+    } else {
+        return repeatedString;
+    }   
 };
 
 // Do not edit below this line
